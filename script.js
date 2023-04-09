21e1a96528f5be1bd18f9a49f65b7a5f51fad495
Fatemeh Mohammadi <ftmmohammadi1805@gmail.com>
2023-04-09 14:05:40 +0430

    Megamenu

---
+++ script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var span = document.querySelector(".myspan")
+    var nav = document.getElementById("myDIV");
+    if (nav.style.display === "none") {
+      nav.style.display = "block";
+    } else {
+      nav.style.display = "none";
+    }
+  }
+
+
+
